7fc47d7f7007067727799cf49641a37185d890c3
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2024-01-22 21:10:43 +0100

    Changed RequestMapping to /auth

--- src/main/java/com/lauro/authorization/server/controller/AuthController.java
+++ src/main/java/com/lauro/authorization/server/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/authentication")
+@RequestMapping("/auth")
 @Slf4j
 public class AuthController {
 
