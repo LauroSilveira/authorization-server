0c19f34c0a85997cc9071b6ff261ef7355eaf092
Lauro Silveira <lauro.silveira@outlook.com.br>
2025-02-03 00:27:01 +0100

    Removed unsed imports

--- src/main/java/com/lauro/authorization/server/config/AuthorizationSecurityConfig.java
+++ src/main/java/com/lauro/authorization/server/config/AuthorizationSecurityConfig.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,10 +24,7 @@ import org.springframework.security.oauth2.server.authorization.token.OAuth2Toke
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-
 import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
-
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
